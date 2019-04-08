d38635d9161c630f267d4394ff2bf1b498c7d6db
joshuab90 <joshbab90@gmail.com>
2019-04-07 20:25:56 -0500

    added permission and authentication classes

--- scavenger_hunt_backend/hunt_app/views.py
+++ scavenger_hunt_backend/hunt_app/views.py
@@ -3,9 +3,11 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 from . import serializers
 from django.http import HttpResponse
 from . import models
+from . import permissions
 
 class HelloApiView(APIView):
 
@@ -74,3 +76,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):   # handles creating, reading, 
     
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
