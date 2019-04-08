4bf03aa26921e7c3fca043ba9c08303012e95507
joshuab90 <joshbab90@gmail.com>
2019-04-08 13:40:20 -0500

    changed URL to signup

--- scavenger_hunt_backend/hunt_app/urls.py
+++ scavenger_hunt_backend/hunt_app/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('profile', views.UserProfileViewSet)
+router.register('signup', views.UserProfileViewSet)
 router.register('login', views.LoginViewSet, basename='login')
 
 urlpatterns = [
