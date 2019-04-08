68a1bfe7978aaad0047c286cc18dc3fd2e6f2676
joshuab90 <joshbab90@gmail.com>
2019-04-07 19:46:24 -0500

    Added viewset and router

--- scavenger_hunt_backend/hunt_app/urls.py
+++ scavenger_hunt_backend/hunt_app/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     url('hello-view/', views.HelloApiView.as_view()),
+    url('', include(router.urls))
 ]
--- scavenger_hunt_backend/hunt_app/views.py
+++ scavenger_hunt_backend/hunt_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
+from django.http import HttpResponse
 
 class HelloApiView(APIView):
     'Test API view'
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None): #  returns list of APIList features
 
@@ -15,3 +21,31 @@ class HelloApiView(APIView):
       ]
 
       return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None): # updates object
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None): # only updates fields required or provided in the request
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method':'delete'})
+
+class HelloViewSet(viewsets.ViewSet):  # test API Viewset  
+    def list(self, request):
+        a_viewset = [
+          'Uses actions (list, create, retrieve, update, partial_update)',
+          'Automatically maps to URLs using Routers',
+          'Provides more functionality with less code.'
+        ]
+        return Response({'message':'Hello!', 'a_viewset': a_viewset})
