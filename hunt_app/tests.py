0e2eca3f7d0b6a1a9f693c79426c59a38abc2ea6
joshuab90 <joshbab90@gmail.com>
2019-04-12 15:16:39 -0500

    testing signup page for 200 response

--- hunt_app/tests.py
+++ hunt_app/tests.py
@@ -1,11 +1,35 @@
 from django.http import HttpRequest
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
 
 from . import views
 
-class ApiPageTest(SimpleTestCase):
+class ApiPageTest(TestCase):
 
     def test_api_status_code(self):
         response = self.client.get('/api/')
         self.assertEquals(response.status_code, 200)
+
+class SignupPageTest(TestCase):
+
+    def test_api_status_code(self):
+        response = self.client.get('/api/signup/')
+        self.assertEquals(response.status_code, 200)
+
+# class ApiPageTest(SimpleTestCase):
+
+#     def test_api_status_code(self):
+#         response = self.client.get('/api/')
+#         self.assertEquals(response.status_code, 200)
+
+# class ApiPageTest(SimpleTestCase):
+
+#     def test_api_status_code(self):
+#         response = self.client.get('/api/')
+#         self.assertEquals(response.status_code, 200)
+
+# class ApiPageTest(SimpleTestCase):
+
+#     def test_api_status_code(self):
+#         response = self.client.get('/api/')
+#         self.assertEquals(response.status_code, 200)
