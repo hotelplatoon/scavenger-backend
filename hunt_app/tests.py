9f964caa35b56449930165846974f24cf370ee2a
joshuab90 <joshbab90@gmail.com>
2019-04-12 15:10:40 -0500

    created first test - api page testing for 200 response

--- hunt_app/tests.py
+++ hunt_app/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+from . import views
+
+class ApiPageTest(SimpleTestCase):
+
+    def test_api_status_code(self):
+        response = self.client.get('/api/')
+        self.assertEquals(response.status_code, 200)
